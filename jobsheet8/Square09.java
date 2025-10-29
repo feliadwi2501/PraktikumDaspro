9774bafb933f1bbf53493edb0f1013ce84d186df
“feliadwi2501” <feliadwi2501@gmail.com>
2025-10-29 09:34:39 +0700

    percobaan 2

---
+++ jobsheet8/Square09.java
@@ -0,0 +1,18 @@
+package jobsheet8;
+import java.util.Scanner;
+public class Square09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+         System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+
+        for(int iOuter=1; iOuter<=n; iOuter++){
+            for(int i=0; i<=n; i++){
+            System.out.print("*");
+    }
+    System.out.println();
+}
+}
+}
+
